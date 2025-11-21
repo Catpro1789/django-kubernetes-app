49d5bbba938008d830000dcdb0d8aac9398eadcd
Catpro1789 <113348961+Catpro1789@users.noreply.github.com>
2025-11-21 15:50:02 +0300

    Update urls.py

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.http import HttpResponse
-from django_app.settings import get_hostname
+from .settings import get_hostname
 
 def index(request):
-    return HttpResponse(f"Hello! Hostname: {get_hostname()}")
+    # Показываем hostname пода
+    return HttpResponse(f"Hostname: {get_hostname()}")
 
 urlpatterns = [
     path('', index),
 ]
-
