664d5be3f770a28616b24b4328bb475f88b7cdcf
Catpro1789 <113348961+Catpro1789@users.noreply.github.com>
2025-11-27 15:11:11 +0300

    Update settings.py

--- django_app/settings.py
+++ django_app/settings.py
@@ -1,31 +1,16 @@
 import os
 import socket
 
-SECRET_KEY = os.getenv("secret_key", "insecure")
-
-DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
-
-INSTALLED_APPS = [
-    "django.contrib.staticfiles",
-]
-
-ROOT_URLCONF = "django_app.urls"
-WSGI_APPLICATION = "django_app.wsgi.application"
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "myapp"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'myapp'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
-STATIC_URL = "/static/"
-STATIC_ROOT = "/app/static"
 
 def get_hostname():
     return socket.gethostname()
