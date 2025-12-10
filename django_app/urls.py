7caa3afdd91a00fd773769938885542f91963104
Catpro1789 <113348961+Catpro1789@users.noreply.github.com>
2025-12-10 16:29:20 +0300

    Update urls.py

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse
 from django.urls import path
-from django_app.settings import get_hostname
+from django.http import HttpResponse
+from .settings import get_hostname
 
 def index(request):
-    return HttpResponse(f"Hostname: {get_hostname()}")
+    return HttpResponse(f"Hello from pod: {get_hostname()}")
 
 urlpatterns = [
-    path("", index),
-    path("health/", lambda r: HttpResponse("OK")),
+    path('', index),
 ]
