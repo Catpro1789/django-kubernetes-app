010ab30d9835741e236b7bc91c502ad89534e2dd
Catpro1789 <113348961+Catpro1789@users.noreply.github.com>
2025-12-10 16:29:56 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python3
-import os, sys
+#!/usr/bin/env python
+import os
+import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
-from django.core.management import execute_from_command_line
-
-execute_from_command_line(sys.argv)
+if __name__ == '__main__':
+    main()
