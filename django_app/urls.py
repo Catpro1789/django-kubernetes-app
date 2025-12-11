2839d569caa98ccd66095402c6cd359239f4416a
Catpro1789 <113348961+Catpro1789@users.noreply.github.com>
2025-12-11 11:43:58 +0300

    Update urls.py

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 from .settings import get_hostname
@@ -6,5 +7,8 @@ def index(request):
     return HttpResponse(f"Hello from pod: {get_hostname()}")
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', index),
+    path('health/', lambda request: HttpResponse("OK")),
+    path('metrics/', lambda request: HttpResponse("Prometheus metrics")),
 ]
